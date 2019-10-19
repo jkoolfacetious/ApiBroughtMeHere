7b837a878c31d9be7a1562b06ed4a06cb893a9da
jesse <jesse.o.dickson@leidos.com>
2019-10-19 16:12:49 -0500

    added auth token class and config swagger

---
+++ ApiBMH_DataManager/App_Start/AuthorizationOperationFilter.cs
@@ -0,0 +1,23 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace ApiBMH_DataManager.App_Start
+{
+    public class AuthorizationOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            // add parameter to every operation
+            if (operation.parameters == null)
+            {
+                operation.parameters = new List<Parameter>();
+            }
+
+
+        }
+    }
+}
