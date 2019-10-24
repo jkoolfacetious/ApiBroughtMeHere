076d1b85b04f3f5fec2166bc50a80e47baf3a531
jesse <jesse.o.dickson@leidos.com>
2019-10-24 16:12:37 -0500

    Created Bootstrap to Launch MVVM

---
+++ ApiBMHDesktopUserInterface/bootstrapBootstraps.cs
@@ -0,0 +1,24 @@
+﻿using ApiBMHDesktopUserInterface.ViewModels;
+using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ApiBMHDesktopUserInterface
+{
+   public class bootstrapBootstraps : BootstrapperBase
+    {
+        public bootstrapBootstraps()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
