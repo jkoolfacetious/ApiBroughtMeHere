08210252f043ffe93426a7cd5cd98e890999c7a2
jesse <jesse.o.dickson@leidos.com>
2019-11-04 23:26:03 -0600

    added username and password code blocks

--- ApiBMHDesktopUserInterface/ViewModels/LoginViewModel.cs
+++ ApiBMHDesktopUserInterface/ViewModels/LoginViewModel.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 
 namespace ApiBMHDesktopUserInterface.ViewModels
 {
-    class LoginViewModel
+    public class LoginViewModel : Screen
     {
+        private String _userName;
+        private String _password;
+
+        public int MyProperty
+        {
+            get { return _userName; }
+            set { _userName = value; }
+        }
+
+
     }
 }
--- ApiBMHDesktopUserInterface/ViewModels/ShellViewModel.cs
+++ ApiBMHDesktopUserInterface/ViewModels/ShellViewModel.cs
@@ -10,6 +10,13 @@ namespace ApiBMHDesktopUserInterface.ViewModels
     public class ShellViewModel : Conductor <object>
     {
 
+        private LoginViewModel _loginVM;
+
+        public ShellViewModel(LoginViewModel loginVM)
+        {
+            _loginVM = loginVM;
+            ActivateItem(_loginVM);
+        }
 
     }
 }
