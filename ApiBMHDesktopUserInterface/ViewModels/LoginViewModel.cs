00f2db7177c125e3e880b3e158407ea7c36b5320
jesse <jesse.o.dickson@leidos.com>
2019-11-05 00:41:18 -0600

    finished login form

--- ApiBMHDesktopUserInterface/ViewModels/LoginViewModel.cs
+++ ApiBMHDesktopUserInterface/ViewModels/LoginViewModel.cs
@@ -9,15 +9,53 @@ namespace ApiBMHDesktopUserInterface.ViewModels
 {
     public class LoginViewModel : Screen
     {
-        private String _userName;
-        private String _password;
+        private string _userName;
+        private string _password;
 
-        public int MyProperty
+        //getting the user's  username  and if not no go
+        public string UserName
         {
             get { return _userName; }
-            set { _userName = value; }
+            set 
+            {
+                _userName = value;
+                NotifyOfPropertyChange(() => UserName);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
         }
 
+
+        //getting the user's password if not no go
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        //checking to see if the user & pass have something in them before you can click login button
+        public bool CanLogIn
+        {
+            get
+            {
+              bool output = false;
+
+                if (UserName?.Length > 0 && Password?.Length > 0)
+                {
+                    output = true;
+                }
+                return output;
+            }
+        }
+
+        public void LogIn()
+        {
+           
+        }
 
     }
 }
--- ApiBMHDesktopUserInterface/bootstrapBootstraps.cs
+++ ApiBMHDesktopUserInterface/bootstrapBootstraps.cs
@@ -1,4 +1,5 @@
-﻿using ApiBMHDesktopUserInterface.ViewModels;
+﻿using ApiBMHDesktopUserInterface.Helpers;
+using ApiBMHDesktopUserInterface.ViewModels;
 using Caliburn.Micro;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace ApiBMHDesktopUserInterface
 {
@@ -18,6 +20,11 @@ namespace ApiBMHDesktopUserInterface
         public bootstrapBootstraps()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+            PasswordBoxHelper.BoundPasswordProperty,
+            "Password",
+            "PasswordChanged");
         }
 
         protected override void Configure()
